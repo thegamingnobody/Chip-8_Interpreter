52433328ad0b92ac03238cc8dca88266f1afd982
Seppe Mestdagh <mestdaghseppe@gmail.com>
2025-03-06 17:40:52 +0100

    Initial commit

---
+++ Chip-8_Interpreter.cpp
@@ -0,0 +1,12 @@
+﻿// Chip-8_Interpreter.cpp : Defines the entry point for the application.
+//
+
+#include "Chip-8_Interpreter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
